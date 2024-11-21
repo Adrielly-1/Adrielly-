fde9f6afc6972374f072d50aee1d71f960c28aba
adrielly <adrielly@gmail.com>
2024-11-20 23:48:58 -0300

    ajustes

--- filmes/api/viewsets.py
+++ filmes/api/viewsets.py
@@ -7,11 +7,12 @@ from filmes.api import serializer
 from filmes.models import Filme
 from filmes.api.serializer import FilmesSerializer
 
+
 class FilmesViewSets(APIView):
     def get(self, request, id=None):
         if id:
             filme = get_object_or_404(Filme, id=id)
-            serializer = FilmeSerializer(filme)
+            serializer = FilmesSerializer(filme)
             return Response(serializer.data)
         items = Filme.objects.all()
         serializer = FilmesSerializer(items, many= True)
@@ -22,7 +23,7 @@ class FilmesViewSets(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(seralizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         item = get_object_or_404(Filme, id=id)
